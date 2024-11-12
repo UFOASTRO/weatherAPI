f99785f1e4f9cdb7923634638240aafbf40bdeec
Astro <nifemibello13@gmail.com>
2024-11-12 14:27:09 +0100

    add first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,js}",  // Adjust this to your project's file structure
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
